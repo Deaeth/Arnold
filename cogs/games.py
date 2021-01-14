f0795e467464bc04a5ee2dba879d94e34cfadb49
urmom69 <hemanthkumar01@outlook.com>
2021-01-15 00:36:54 +0530

    minor change

--- cogs/games.py
+++ cogs/games.py
@@ -154,7 +154,7 @@ class Games(commands.Cog):
         return
 
 @commands.command(name = "slotmachine")
-async def slotmachine(ctx):
+async def slotmachine(self,ctx):
     outcomes = ["<:OB_ratdogsmile:791772548144693268>","<:OB_ratdog:737111061722955807>","<:OB_mike:737105902720647258>","<:OB_monkastare:755857538632777899>"]
     randomOutcomes = [random.choice(outcomes), random.choice(outcomes), random.choice(outcomes)]
     msg = ""
